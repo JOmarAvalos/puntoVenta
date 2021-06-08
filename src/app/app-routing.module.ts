4d07e57d72f184dfa761a85c37df12b4d2c24a91
Omar Avalos <omar.avalos.m@gmail.com>
2021-06-08 18:15:25 -0500

    Template ajuste v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot( routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
